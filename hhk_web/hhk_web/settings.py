27d71ec93d45f3726efc1ba8715413404d45507f
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-09 21:08:06 +0300

    Added blog and project apps

--- hhk_web/hhk_web/settings.py
+++ hhk_web/hhk_web/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'project.apps.ProjectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
