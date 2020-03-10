61d24feb746130af363b86dddd4e1d1ddbc4ee05
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-10 22:58:39 +0300

    Added paths of static pages

--- hhk_web/hhk_web/urls.py
+++ hhk_web/hhk_web/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("blog/", include("blog.urls")),
     path("projects/", include("project.urls")),
+    path("about/", blog_views.about, name="about"),
+    path("contact/", blog_views.contact, name="contact"),
+    path("", blog_views.home, name="home"),
 ]
