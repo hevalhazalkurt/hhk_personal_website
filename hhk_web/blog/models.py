8df9f12d19bb4e7046f10affae8e5a085952ae56
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-10 22:58:01 +0300

    Created BlogPost model

--- hhk_web/blog/models.py
+++ hhk_web/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=250)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = "Heval Hazal Kurt"
+
+
+    def __str__(self):
+        return self.title
