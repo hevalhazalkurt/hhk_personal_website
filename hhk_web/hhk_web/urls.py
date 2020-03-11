8f0530bdadd0609a23ab41db5f2ea4b32fc3eb4d
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-11 21:54:11 +0300

    Added image url path setting

--- hhk_web/hhk_web/urls.py
+++ hhk_web/hhk_web/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog import views as blog_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,4 @@ urlpatterns = [
     path("about/", blog_views.about, name="about"),
     path("contact/", blog_views.contact, name="contact"),
     path("", blog_views.home, name="home"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
