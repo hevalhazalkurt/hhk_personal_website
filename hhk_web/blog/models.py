36d6f0701e3b0114e3de3376b2ebbff32a33f6b1
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-11 21:56:19 +0300

    Added image field

--- hhk_web/blog/models.py
+++ hhk_web/blog/models.py
@@ -3,8 +3,10 @@ from django.utils import timezone
 
 
 class BlogPost(models.Model):
+    image = models.ImageField(upload_to="images/")
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = "Heval Hazal Kurt"
