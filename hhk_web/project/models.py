1feb4d78b6d0f9a735567145e79d819ba1eae88d
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-11 21:56:07 +0300

    Added image and content field

--- hhk_web/project/models.py
+++ hhk_web/project/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 class ProjectPost(models.Model):
+    image = models.ImageField(upload_to="images/")
     title = models.CharField(max_length=100)
     client = models.CharField(max_length=100)
     position = models.CharField(max_length=200)
     link = models.URLField(max_length=250)
     date_posted = models.DateTimeField(default=timezone.now)
     summary = models.CharField(max_length=250)
+    content = models.TextField()
+    category = models.CharField(max_length=250)
 
 
     def __str__(self):
