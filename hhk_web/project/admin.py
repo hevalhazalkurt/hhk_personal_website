f694f3f769095b135b753f3d6913df163ef50a46
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-23 16:56:22 +0300

    Added project category

--- hhk_web/project/admin.py
+++ hhk_web/project/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ProjectPost
+from .models import ProjectPost, ProjectCategory
 
 
 admin.site.register(ProjectPost)
+admin.site.register(ProjectCategory)
--- hhk_web/project/models.py
+++ hhk_web/project/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
+
+class ProjectCategory(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'project category'
+        verbose_name_plural = 'project categories'
+
+
+    def __str__(self):
+        return self.title
+
+
+
 class ProjectPost(models.Model):
     image = models.ImageField(upload_to="images/")
     title = models.CharField(max_length=100)
@@ -10,7 +26,7 @@ class ProjectPost(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     summary = models.CharField(max_length=250)
     content = models.TextField()
-    category = models.CharField(max_length=250)
+    category = models.ForeignKey(ProjectCategory, on_delete=models.CASCADE)
 
 
     def __str__(self):
