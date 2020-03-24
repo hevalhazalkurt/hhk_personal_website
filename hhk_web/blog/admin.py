1be4fddb177e1292afa486ee6fd67580f28b4ef2
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-24 16:21:24 +0300

    Added auto slug admin field

--- hhk_web/blog/admin.py
+++ hhk_web/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import BlogPost, BlogCategory
 
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(BlogPost)
+admin.site.register(BlogPost, BlogAdmin)
 admin.site.register(BlogCategory)
