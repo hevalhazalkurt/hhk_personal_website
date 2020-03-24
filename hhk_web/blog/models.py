3fd910b4741d44f06ea4274be29e65cbbffa559e
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-24 16:21:57 +0300

    Added absolute url function

--- hhk_web/blog/models.py
+++ hhk_web/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
+
 
 
 class BlogCategory(models.Model):
@@ -21,12 +23,15 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=250)
     category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE)
-    #category = models.CharField(max_length=250)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = "Heval Hazal Kurt"
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, blank = True)
 
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('blog-detail', args=[self.slug])
