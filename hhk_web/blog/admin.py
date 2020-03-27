c8edef57916295913cf72628fe57dffca34e545b
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-27 19:17:51 +0300

    Added blog category and auto slug to the admin panel

--- hhk_web/blog/admin.py
+++ hhk_web/blog/admin.py
@@ -4,5 +4,10 @@ from .models import BlogPost, BlogCategory
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
+
+class BlogCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(BlogPost, BlogAdmin)
-admin.site.register(BlogCategory)
+admin.site.register(BlogCategory, BlogCategoryAdmin)
