9eafa910ffa48240df3d6a2e334313b239a02995
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-27 19:21:23 +0300

    Added category path

--- hhk_web/project/urls.py
+++ hhk_web/project/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProjectPostListView, ProjectDetailView
+from .views import ProjectPostListView, ProjectDetailView, ProjectCategoryListView
 from . import views
 
 
 urlpatterns = [
     path("", ProjectPostListView.as_view(), name="project-home"),
     path("<slug>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("category/<slug>/", ProjectCategoryListView.as_view(), name="project-category"),
 ]
