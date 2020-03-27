9a209c4cf10fa9750dd64e12fbf3f719857f8bab
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-27 19:18:35 +0300

    dded project category and auto slug to the admin panel

--- hhk_web/project/admin.py
+++ hhk_web/project/admin.py
@@ -5,5 +5,10 @@ from .models import ProjectPost, ProjectCategory
 class ProjectAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(ProjectPost, ProjectAdmin)
-admin.site.register(ProjectCategory)
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
