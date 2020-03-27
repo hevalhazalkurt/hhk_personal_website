c29ab40c4883d056d24e0d2d03474373d7705982
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-27 19:21:17 +0300

    Added category path

--- hhk_web/blog/urls.py
+++ hhk_web/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogPostListView, BlogDetailView
+from .views import BlogPostListView, BlogDetailView , BlogCategoryListView
 from . import views
 
 
 urlpatterns = [
     path("", BlogPostListView.as_view(), name="blog-home"),
     path("<slug>/", BlogDetailView.as_view(), name="blog-detail"),
+    path("category/<slug>/", BlogCategoryListView.as_view(), name="blog-category"),
 ]
