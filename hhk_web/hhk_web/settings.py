2ff59079afbe9b3752e5e724cdcef47e7bcc1bef
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-27 19:17:08 +0300

    Added contact form email settings

--- hhk_web/hhk_web/settings.py
+++ hhk_web/hhk_web/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
