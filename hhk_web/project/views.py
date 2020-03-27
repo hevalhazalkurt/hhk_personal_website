8daf437e1beb4d69f372fc3b18a3d87dcc614981
hevalhazalkurt <hevalhazal@gmail.com>
2020-03-27 19:20:42 +0300

    Added blog category view

--- hhk_web/project/views.py
+++ hhk_web/project/views.py
@@ -1,40 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import ProjectPost
+from .models import ProjectPost, ProjectCategory
 
-
-dummy_projects = [
-      {
-          "client": "Company A",
-          "title": "Project 1",
-          "position": "Jr. Python Developer",
-          "category": "Development",
-          "content": "First project content",
-          "date_posted": "March 2, 2019",
-      },
-      {
-          "client": "Company B",
-          "title": "Project 2",
-          "position": "Director",
-          "category": "Filmmaking",
-          "content": "Second project content",
-          "date_posted": "March 5, 2019",
-      },
-      {
-          "client": "Company C",
-          "title": "Project 3",
-          "position": "Producer",
-          "category": "Filmmaking",
-          "content": "Third project content",
-          "date_posted": "March 7, 2019",
-      },
-]
-
-
-def projecthome(request):
-    #context = {"project_posts": dummy_projects}
-    context = {"project_posts": ProjectPost.objects.all()}
-    return render(request, "project/projecthome.html", context)
 
 
 class ProjectPostListView(ListView):
@@ -42,10 +9,22 @@ class ProjectPostListView(ListView):
     template_name = "project/projecthome.html"
     context_object_name = "project_posts"
     ordering = ["-date_posted"]
-    paginate_by = 10
 
 
 
 class ProjectDetailView(DetailView):
     model = ProjectPost
     template_name = "project/project_detail.html"
+
+
+
+
+class ProjectCategoryListView(ListView):
+    model = ProjectPost
+    template_name = "project/category_projects.html"
+    context_object_name = "category_projects"
+
+
+    def get_queryset(self):
+        category = get_object_or_404(ProjectCategory, slug=self.kwargs.get("slug"))
+        return ProjectPost.objects.filter(category_id=category).order_by("-date_posted")
