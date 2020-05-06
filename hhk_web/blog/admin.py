a6184ad4b19a0c1034b93582d974f1ece3b903a7
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-06 22:34:31 +0300

    Deleted prepopulated slug field

--- hhk_web/blog/admin.py
+++ hhk_web/blog/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from .models import BlogPost, BlogCategory
 
-class BlogAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
 
-
-class BlogCategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-
-
-admin.site.register(BlogPost, BlogAdmin)
-admin.site.register(BlogCategory, BlogCategoryAdmin)
+admin.site.register(BlogPost)
+admin.site.register(BlogCategory)
