7b2533ef6f14c548aba4d547734ee57acbf8217c
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-06 22:36:07 +0300

    Added text editor path and changed home path

--- hhk_web/hhk_web/urls.py
+++ hhk_web/hhk_web/urls.py
@@ -7,9 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls")),
+    path("", include("blog.urls")),
     path("projects/", include("project.urls")),
     path("about/", blog_views.about, name="about"),
     path("contact/", blog_views.contact, name="contact"),
-    path("", blog_views.home, name="home"),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
