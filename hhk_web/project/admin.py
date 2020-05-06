aaddbcabccf4b1aa028109eb8df8259011875bd7
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-06 22:34:25 +0300

    Deleted prepopulated slug field

--- hhk_web/project/admin.py
+++ hhk_web/project/admin.py
@@ -2,13 +2,5 @@ from django.contrib import admin
 from .models import ProjectPost, ProjectCategory
 
 
-class ProjectAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-
-
-class ProjectCategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-
-
-admin.site.register(ProjectPost, ProjectAdmin)
-admin.site.register(ProjectCategory, ProjectCategoryAdmin)
+admin.site.register(ProjectPost)
+admin.site.register(ProjectCategory)
