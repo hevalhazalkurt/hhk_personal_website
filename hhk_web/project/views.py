6c43edbcc1e96e64bba269f651c03a7ee28e9f67
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-10 22:16:19 +0300

    Changed project views as templateview

--- hhk_web/project/views.py
+++ hhk_web/project/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import ProjectPost, ProjectCategory
 
 
-
-class ProjectPostListView(ListView):
-    model = ProjectPost
+class ProjectPostListView(TemplateView):
     template_name = "project/projecthome.html"
-    context_object_name = "project_posts"
-    ordering = ["-date_posted"]
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = ProjectPost.objects.all()[::-1]
+        context['project_categories'] = ProjectCategory.objects.all()
+        return context
 
 
 class ProjectDetailView(DetailView):
