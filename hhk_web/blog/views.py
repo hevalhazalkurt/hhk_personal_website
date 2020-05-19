1e60ebc7a959c3157d9dabe9add4f1add6c1d304
hevalhazalkurt <hevalhazal@gmail.com>
2020-05-19 22:10:12 +0300

    Updated paginate by numbers

--- hhk_web/blog/views.py
+++ hhk_web/blog/views.py
@@ -36,7 +36,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_posts'] = BlogPost.objects.order_by('-date_posted')[:3]
-        context['latest_projects'] = ProjectPost.objects.order_by('-date_posted')[:4]
+        context['latest_projects'] = ProjectPost.objects.order_by('-date_posted')[:3]
         context['post_categories'] = BlogCategory.objects.all()
         context['project_categories'] = ProjectCategory.objects.all()
         return context
@@ -48,7 +48,7 @@ class BlogPostListView(ListView):
     template_name = "blog/bloghome.html"
     context_object_name = "blog_posts"
     ordering = ["-date_posted"]
-    paginate_by = 9
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -70,7 +70,7 @@ class BlogCategoryListView(ListView):
     model = BlogPost
     template_name = "blog/category_posts.html"
     context_object_name = "category_posts"
-    paginate_by = 9
+    paginate_by = 5
 
 
     def get_queryset(self):
